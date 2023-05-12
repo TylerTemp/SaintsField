1af0904fcd968563141f04e3e45378e97a04bbe2
TylerTemp <tylertempdev@gmail.com>
2023-05-12 16:24:04 +0800

    Add SepTitle

--- Assets/Scripts/ExtInspector/Samples/MonoExample.cs
+++ Assets/Scripts/ExtInspector/Samples/MonoExample.cs
@@ -3,18 +3,21 @@ using ExtInspector.Standalone;
 using NaughtyAttributes;
 #endif
 using UnityEngine;
+using EColor = ExtInspector.Standalone.EColor;
 
 namespace ExtInspector.Samples
 {
     public class MonoExample : MonoBehaviour
     {
-//         [SerializeField, Standalone.Scene] private string _scene;
-//         [GO] public GameObject _go;
-// #if EXT_INSPECTOR_WITH_NAUGHTY_ATTRIBUTES
-//         [field: Label("TestLabelOverride"), SerializeField] public string Sth { get; private set; }
-// #endif
+        [SerializeField, Standalone.Scene] private string _scene;
+        [GO] public GameObject _go;
+#if EXT_INSPECTOR_WITH_NAUGHTY_ATTRIBUTES
+        [field: Label("TestLabelOverride"), SerializeField] public string Sth { get; private set; }
+#endif
 
         // [field: LabelDec, SerializeField] public string LabelDecTest { get; private set; }
         [Icon("UnityEditor.SceneView"), SerializeField] private string _labelDecTest2;
+
+        [field: SepTitle("TestSep", EColor.Green), SerializeField] public int SepTitleTest { get; private set; }
     }
 }
---
+++ Assets/Scripts/ExtInspector/Standalone/EColor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace ExtInspector.Standalone
+{
+    public enum EColor
+    {
+        Clear,
+        White,
+        Black,
+        Gray,
+        Red,
+        Pink,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Indigo,
+        Violet,
+    }
+
+    public static class EColorExtensions
+    {
+        public static Color32 GetColor(this EColor color)
+        {
+            return color switch
+            {
+                EColor.Clear => new Color32(0, 0, 0, 0),
+                EColor.White => new Color32(255, 255, 255, 255),
+                EColor.Black => new Color32(0, 0, 0, 255),
+                EColor.Gray => new Color32(128, 128, 128, 255),
+                EColor.Red => new Color32(255, 0, 63, 255),
+                EColor.Pink => new Color32(255, 152, 203, 255),
+                EColor.Orange => new Color32(255, 128, 0, 255),
+                EColor.Yellow => new Color32(255, 211, 0, 255),
+                EColor.Green => new Color32(98, 200, 79, 255),
+                EColor.Blue => new Color32(0, 135, 189, 255),
+                EColor.Indigo => new Color32(75, 0, 130, 255),
+                EColor.Violet => new Color32(128, 0, 255, 255),
+                _ => new Color32(0, 0, 0, 255),
+            };
+        }
+    }
+}
