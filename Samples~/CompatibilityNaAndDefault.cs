0dff140fb346d8d6a16aa70ecbdaade28c19d3d2
TylerTemp <tylertempdev@gmail.com>
2023-11-20 10:38:43 +0000

    update example

--- Samples~/CompatibilityNaAndDefault.cs
+++ Samples~/CompatibilityNaAndDefault.cs
@@ -4,15 +4,14 @@ namespace SaintsField.Samples
 {
     public class CompatibilityNaAndDefault : MonoBehaviour
     {
+        [RichLabel("<color=green>+Native</color>"), Range(0, 5)]
+        public float nativeRange;
+        // this wont work. Please put `SaintsField` before other drawers
+        [Range(0, 5), RichLabel("<color=green>+Native</color>")]
+        public float nativeRangeHandled;
+
         // [RichLabel("<color=green>+NA</color>"), NaughtyAttributes.CurveRange(0, 0, 1, 1, NaughtyAttributes.EColor.Green)]
         // public AnimationCurve naCurve;
-        //
-        // [RichLabel("<color=green>+Native</color>"), Range(0, 5)]
-        // public float nativeRange;
-        //
-        // // this wont work. Please put `SaintsField` before other drawers
-        // [Range(0, 5), RichLabel("<color=green>+Native</color>")]
-        // public float nativeRangeHandled;
         //
         // // this wont work too.
         // [NaughtyAttributes.CurveRange(0, 0, 1, 1, NaughtyAttributes.EColor.Green), RichLabel("<color=green>+NA</color>")]
