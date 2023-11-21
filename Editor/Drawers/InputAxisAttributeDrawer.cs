0b59e48845256b3c0a8aea564de4d4d6f16f6bae
TylerTemp <tylertempdev@gmail.com>
2023-11-21 06:55:35 +0000

    input axis

---
+++ Editor/Drawers/InputAxisAttributeDrawer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SaintsField.Editor.Core;
+using UnityEditor;
+using UnityEngine;
+
+namespace SaintsField.Editor.Drawers
+{
+    [CustomPropertyDrawer(typeof(InputAxisAttribute))]
+    public class InputAxisAttributeDrawer: SaintsPropertyDrawer
+    {
+        private IReadOnlyList<string> _axisNames;
+
+        private static IReadOnlyList<string> GetAxisNames()
+        {
+            SerializedObject inputAssetSettings = new SerializedObject(AssetDatabase.LoadAssetAtPath<Object>("ProjectSettings/InputManager.asset"));
+            SerializedProperty axesProperty = inputAssetSettings.FindProperty("m_Axes");
+            List<string> axisNames = new List<string>();
+            for (int index = 0; index < axesProperty.arraySize; index++)
+            {
+                axisNames.Add(axesProperty.GetArrayElementAtIndex(index).FindPropertyRelative("m_Name").stringValue);
+            }
+
+            return axisNames;
+        }
+
+        protected override float GetFieldHeight(SerializedProperty property, GUIContent label,
+            ISaintsAttribute saintsAttribute, bool hasLabelWidth)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        protected override void DrawField(Rect position, SerializedProperty property, GUIContent label, ISaintsAttribute saintsAttribute)
+        {
+            _axisNames ??= GetAxisNames();
+
+            int index = IndexOf(_axisNames, property.stringValue);
+            // ReSharper disable once ConvertToUsingDeclaration
+            using(EditorGUI.ChangeCheckScope changeCheck = new EditorGUI.ChangeCheckScope())
+            {
+                int newIndex = EditorGUI.Popup(position, label, index, _axisNames.Select(each => new GUIContent(each)).ToArray());
+                if (changeCheck.changed)
+                {
+                    property.stringValue = _axisNames[newIndex];
+                }
+            }
+        }
+
+        private static int IndexOf(IEnumerable<string> axisNames, string value)
+        {
+            // int index = Array.IndexOf(scenes, scene);
+            // return index == -1? 0: index;
+            foreach ((string axisName, int index) in axisNames.Select((axisName, index) => (axisName, index)))
+            {
+                if (axisName == value)
+                {
+                    return index;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Runtime/InputAxisAttribute.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace SaintsField
+{
+    public class InputAxisAttribute: PropertyAttribute, ISaintsAttribute
+    {
+        public SaintsAttributeType AttributeType => SaintsAttributeType.Field;
+
+        public string GroupBy => "__LABEL__FIELD__";
+    }
+}
