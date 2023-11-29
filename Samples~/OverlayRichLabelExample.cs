423a1a4eebfe238a89ca2f3abadee4432f645ff2
TylerTemp <tylertempdev@gmail.com>
2023-11-29 09:37:25 +0000

    test callback

--- Samples~/OverlayRichLabelExample.cs
+++ Samples~/OverlayRichLabelExample.cs
@@ -9,7 +9,23 @@ namespace SaintsField.Samples
         [OverlayRichLabel("<color=grey>/int", padding: 1)] public int count = int.MinValue;
         [OverlayRichLabel("<color=grey>/long", padding: 1)] public long longInt = long.MinValue;
         [OverlayRichLabel("<color=grey>suffix", end: true)] public string atEnd;
+        [OverlayRichLabel(nameof(TakeAGuess), isCallback: true)] public int guess;
         [Space]
         [OverlayRichLabel("not ok", end: true)] public GameObject notSupported;
+
+        public string TakeAGuess()
+        {
+            if(guess > 20)
+            {
+                return "<color=red>too high";
+            }
+
+            if (guess < 10)
+            {
+                return "<color=blue>too low";
+            }
+
+            return "<color=green>acceptable!";
+        }
     }
 }
