103e03fa3eee2be41f9ed52fd6a6d7f284d76816
TylerTemp <tylertempdev@gmail.com>
2023-12-01 10:58:01 +0000

    fix runtime

---
+++ Editor/Utils/EMessageTypeExtenstion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEditor;
+
+namespace SaintsField.Editor.Utils
+{
+    public static class EMessageTypeExtensions
+    {
+        public static MessageType GetMessageType(this EMessageType messageType) => messageType switch
+        {
+            EMessageType.None => MessageType.None,
+            EMessageType.Info => MessageType.Info,
+            EMessageType.Warning => MessageType.Warning,
+            EMessageType.Error => MessageType.Error,
+            _ => throw new ArgumentOutOfRangeException(nameof(messageType), messageType, null)
+        };
+    }
+}
--- Runtime/EMessageType.cs
+++ Runtime/EMessageType.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using UnityEditor;
-
-namespace SaintsField
+﻿namespace SaintsField
 {
     public enum EMessageType
     {
@@ -21,17 +18,5 @@ namespace SaintsField
         ///   <para>Error message.</para>
         /// </summary>
         Error,
-    }
-
-    public static class EMessageTypeExtensions
-    {
-        public static MessageType GetMessageType(this EMessageType messageType) => messageType switch
-        {
-            EMessageType.None => MessageType.None,
-            EMessageType.Info => MessageType.Info,
-            EMessageType.Warning => MessageType.Warning,
-            EMessageType.Error => MessageType.Error,
-            _ => throw new ArgumentOutOfRangeException(nameof(messageType), messageType, null)
-        };
     }
 }
