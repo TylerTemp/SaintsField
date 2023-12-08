20280ce55f92b9ce6d9d26fe20fac591b8d5f656
TylerTemp <tylertempdev@gmail.com>
2023-12-08 07:23:21 +0000

    fix sample

--- Samples~/Scripts/CompatibilityNaAndDefault.cs
+++ Samples~/Scripts/CompatibilityNaAndDefault.cs
@@ -1,46 +1,45 @@
 // using MarkupAttributes;
-
 using UnityEngine;
 
-namespace SaintsField.Samples.Scripts
+namespace SaintsField.Samples
 {
     public class CompatibilityNaAndDefault : MonoBehaviour
     {
-        [RichLabel("<color=green>+Native</color>"), PropRange(0, 5)]
+        [RichLabel("<color=green>+Native</color>"), Range(0, 5)]
         public float nativeRange;
         // this wont work. Please put `SaintsField` before other drawers
-        [PropRange(0, 5), RichLabel("<color=green>+Native</color>")]
+        [Range(0, 5), RichLabel("<color=green>+Native</color>")]
         public float nativeRangeHandled;
 
         // [RichLabel("<color=green>+NA</color>"), NaughtyAttributes.Label(" "), NaughtyAttributes.CurveRange(0, 0, 1, 1, NaughtyAttributes.EColor.Green)]
         // public AnimationCurve naCurve;
+        // //
+        // // // this wont work too.
+        // // [NaughtyAttributes.CurveRange(0, 0, 1, 1, NaughtyAttributes.EColor.Green), RichLabel("<color=green>+NA</color>")]
+        // // public AnimationCurve naCurveHandled;
         //
-        // // this wont work too.
-        // [NaughtyAttributes.CurveRange(0, 0, 1, 1, NaughtyAttributes.EColor.Green), RichLabel("<color=green>+NA</color>")]
-        // public AnimationCurve naCurveHandled;
-
-        [Tooltip("TT1")]
-        public int tt1;
-
-        [RichLabel("Rich"), Tooltip("TT2"), MinMaxSlider(0, 10)] public Vector2 tt2;
-
-        // [TabScope("Tab Scope", "Left|Right", box: true)]
-        // [Tab("./Left")]
-        // [MinValue(10), AboveRichLabel("Test Cap")]
-        // public int one;
+        // [Tooltip("TT1")]
+        // public int tt1;
         //
-        // [Tab("../Right")]
-        // [MinMaxSlider(0, nameof(one)), RichLabel("MinMaxSlider")]
-        // public Vector2 two;
-        // public int three;
-
-        // [Box("Group")]
-        // public int one;
-        // [TitleGroup("Group/Nested Group 1")]
-        // public int two;
-        // public int three;
-        // [TitleGroup("Group/Nested Group 2")]
-        // public int four;
-        // public int five;
+        // [RichLabel("Rich"), Tooltip("TT2"), MinMaxSlider(0, 10)] public Vector2 tt2;
+        //
+        // // [TabScope("Tab Scope", "Left|Right", box: true)]
+        // // [Tab("./Left")]
+        // // [MinValue(10), AboveRichLabel("Test Cap")]
+        // // public int one;
+        // //
+        // // [Tab("../Right")]
+        // // [MinMaxSlider(0, nameof(one)), RichLabel("MinMaxSlider")]
+        // // public Vector2 two;
+        // // public int three;
+        //
+        // // [Box("Group")]
+        // // public int one;
+        // // [TitleGroup("Group/Nested Group 1")]
+        // // public int two;
+        // // public int three;
+        // // [TitleGroup("Group/Nested Group 2")]
+        // // public int four;
+        // // public int five;
     }
 }
