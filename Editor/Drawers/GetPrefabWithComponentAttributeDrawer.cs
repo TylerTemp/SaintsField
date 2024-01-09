23549f2e41a215afca2ff57efc96bba43676c0a3
tyler_huangling <tyler@example.com>
2024-01-09 09:08:25 +0000

    GetPrefabWithComponent

--- Editor/Drawers/GetPrefabWithComponentAttributeDrawer.cs
+++ Editor/Drawers/GetPrefabWithComponentAttributeDrawer.cs
@@ -3,12 +3,14 @@ using SaintsField.Editor.Core;
 using SaintsField.Editor.Utils;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace SaintsField.Editor.Drawers
 {
     [CustomPropertyDrawer(typeof(GetPrefabWithComponentAttribute))]
     public class GetPrefabWithComponentAttributeDrawer: SaintsPropertyDrawer
     {
+        #region IMGUI
         private string _error = "";
 
         protected override float GetPostFieldWidth(Rect position, SerializedProperty property, GUIContent label, ISaintsAttribute saintsAttribute) => 0;
@@ -16,11 +18,31 @@ namespace SaintsField.Editor.Drawers
         protected override bool DrawPostFieldImGui(Rect position, SerializedProperty property, GUIContent label, ISaintsAttribute saintsAttribute,
             bool valueChanged)
         {
-            _error = "";
+            (string error, UnityEngine.Object result) = DoCheckComponent(property, saintsAttribute);
+            if (error != "")
+            {
+                _error = error;
+                return false;
+            }
+            if(result != null)
+            {
+                SetValueChanged(property);
+            }
+            return true;
+        }
 
+        protected override bool WillDrawBelow(SerializedProperty property,
+            ISaintsAttribute saintsAttribute) => _error != "";
+
+        protected override float GetBelowExtraHeight(SerializedProperty property, GUIContent label, float width, ISaintsAttribute saintsAttribute) => _error == ""? 0: ImGuiHelpBox.GetHeight(_error, width, EMessageType.Error);
+        protected override Rect DrawBelow(Rect position, SerializedProperty property, GUIContent label, ISaintsAttribute saintsAttribute) => _error == ""? position: ImGuiHelpBox.Draw(position, _error, EMessageType.Error);
+        #endregion
+
+        private static (string error, UnityEngine.Object result) DoCheckComponent(SerializedProperty property, ISaintsAttribute saintsAttribute)
+        {
             if (property.objectReferenceValue != null)
             {
-                return false;
+                return ("", null);
             }
 
             GetPrefabWithComponentAttribute getPrefabWithComponentAttribute = (GetPrefabWithComponentAttribute) saintsAttribute;
@@ -28,8 +50,7 @@ namespace SaintsField.Editor.Drawers
 
             if (getPrefabWithComponentAttribute.CompType == typeof(GameObject))
             {
-                _error = "You can not use GetPrefabWithComponentAttribute with GameObject type";
-                return false;
+                return ("You can not use GetPrefabWithComponentAttribute with GameObject type", null);
             }
 
             Type type = getPrefabWithComponentAttribute.CompType ?? fieldType;
@@ -64,8 +85,7 @@ namespace SaintsField.Editor.Drawers
 
             if (prefabWithComponent == null)
             {
-                _error = $"No {type} found with prefab";
-                return false;
+                return ($"No {type} found with prefab", null);
             }
 
             UnityEngine.Object result = prefabWithComponent;
@@ -83,14 +103,62 @@ namespace SaintsField.Editor.Drawers
             }
 
             property.objectReferenceValue = result;
-            SetValueChanged(property);
-            return true;
+            return ("", result);
         }
 
-        protected override bool WillDrawBelow(SerializedProperty property,
-            ISaintsAttribute saintsAttribute) => _error != "";
+        #region UIToolkit
 
-        protected override float GetBelowExtraHeight(SerializedProperty property, GUIContent label, float width, ISaintsAttribute saintsAttribute) => _error == ""? 0: ImGuiHelpBox.GetHeight(_error, width, EMessageType.Error);
-        protected override Rect DrawBelow(Rect position, SerializedProperty property, GUIContent label, ISaintsAttribute saintsAttribute) => _error == ""? position: ImGuiHelpBox.Draw(position, _error, EMessageType.Error);
+        private static string NamePlaceholder(SerializedProperty property, int index) =>
+            $"{property.propertyPath}_{index}__GetPrefabWithComponent";
+
+        protected override VisualElement CreatePostFieldUIToolkit(SerializedProperty property,
+            ISaintsAttribute saintsAttribute, int index, VisualElement container, object parent,
+            Action<object> onChange)
+        {
+#if SAINTSFIELD_DEBUG && SAINTSFIELD_DEBUG_DRAW_PROCESS_GET_PREFAB_WITH_COMPONENT
+            Debug.Log($"GetPrefabWithComponent DrawPostFieldUIToolkit for {property.propertyPath}");
+#endif
+            (string error, UnityEngine.Object result) = DoCheckComponent(property, saintsAttribute);
+            if (error != "")
+            {
+                return new VisualElement
+                {
+                    style =
+                    {
+                        width = 0,
+                    },
+                    name = NamePlaceholder(property, index),
+                    userData = error,
+                };
+            }
+
+            property.serializedObject.ApplyModifiedProperties();
+
+            onChange?.Invoke(result);
+
+            return new VisualElement
+            {
+                style =
+                {
+                    width = 0,
+                },
+                name = NamePlaceholder(property, index),
+                userData = "",
+            };
+        }
+
+        // NOTE: ensure the post field is added to the container!
+        protected override VisualElement CreateBelowUIToolkit(SerializedProperty property,
+            ISaintsAttribute saintsAttribute, int index, VisualElement container, object parent)
+        {
+            string error = (string)(container.Q<VisualElement>(NamePlaceholder(property, index))!.userData ?? "");
+#if SAINTSFIELD_DEBUG && SAINTSFIELD_DEBUG_DRAW_PROCESS_GET_PREFAB_WITH_COMPONENT
+            Debug.Log($"GetPrefabWithComponent error {error}");
+#endif
+            return string.IsNullOrEmpty(error)
+                ? null
+                : new HelpBox(_error, HelpBoxMessageType.Error);
+        }
+        #endregion
     }
 }
--- Samples~/Scripts/GetPrefabWithComponentExample.cs
+++ Samples~/Scripts/GetPrefabWithComponentExample.cs
@@ -4,10 +4,10 @@ namespace SaintsField.Samples.Scripts
 {
     public class GetPrefabWithComponentExample: MonoBehaviour
     {
-        [GetPrefabWithComponent] public Dummy dummy;
+        [GetPrefabWithComponent] public Dummy myDummy;
         // get the prefab itself
-        [GetPrefabWithComponent(compType: typeof(Dummy))] public GameObject dummyPrefab;
+        [GetPrefabWithComponent(compType: typeof(Dummy))] public GameObject myDummyPrefab;
         // works so good with `FieldType`
-        [GetPrefabWithComponent(compType: typeof(Dummy)), FieldType(typeof(Dummy))] public GameObject dummyPrefabFieldType;
+        [GetPrefabWithComponent(compType: typeof(Dummy)), FieldType(typeof(Dummy))] public GameObject myDummyPrefabFieldType;
     }
 }
