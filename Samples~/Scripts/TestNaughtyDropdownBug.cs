b5a11f57778a8ece34c6f26e5e62801c6ac2769a
TylerTemp <tylertempdev@gmail.com>
2024-01-26 11:07:28 +0000

    test issue

---
+++ Samples~/Scripts/TestNaughtyDropdownBug.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaintsField.Samples.Scripts
+{
+    public class TestNaughtyDropdownBug : MonoBehaviour
+    {
+        [SerializeField] private TestClass testClass;
+        [SerializeField] private TestStruct testStruct;
+    }
+
+    public static class TestStatic
+    {
+        public static DropdownList<string> TestDropdown = new DropdownList<string>{
+            { "Default", "Default" },
+            { "Value A", "Value A" },
+            { "Value B", "Value B" },
+        };
+    }
+
+    [Serializable]
+    public class TestClass
+    {
+        [SerializeField, Dropdown(nameof(drop))] private string test;
+        public DropdownList<string> drop => TestStatic.TestDropdown;
+    }
+
+    [Serializable]
+    public struct TestStruct
+    {
+        [SerializeField, Dropdown(nameof(drop))] private string test;
+        public DropdownList<string> drop => TestStatic.TestDropdown;
+    }
+}
