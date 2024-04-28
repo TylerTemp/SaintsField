e0250fa0788d74207c2d7a8cc7bd1a337e00dc4b
TylerTemp <tylertempdev@gmail.com>
2024-04-28 12:00:04 +0100

    looking into unity-base-field__aligned

---
+++ Samples~/Scripts/IssueAndTesting/UIKit/BaseFieldAligned.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace SaintsField.Samples.Scripts.IssueAndTesting.UIKit
+{
+    public class BaseFieldAligned : MonoBehaviour
+    {
+        public string s;
+        public string itsALongRideForPeopleWithNothingToThinkAbout;
+
+        [UIToolkit] public string sl;
+        [UIToolkit] public string thereIsSomeGoodNewsForPeopleWhoLoveBadNews;
+    }
+}
