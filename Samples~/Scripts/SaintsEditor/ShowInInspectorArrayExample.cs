d6ad610bb7c80b384751aa6a493267f0bf3e41f8
TylerTemp <tylertempdev@gmail.com>
2024-05-09 03:47:28 +0100

    #22 UI Toolkit Property OK

--- Samples~/Scripts/SaintsEditor/ShowInInspectorArrayExample.cs
+++ Samples~/Scripts/SaintsEditor/ShowInInspectorArrayExample.cs
@@ -5,21 +5,17 @@ namespace SaintsField.Samples.Scripts.SaintsEditor
 {
     public class ShowInInspectorArrayExample : SaintsMonoBehavior
     {
-        // public Color[] colors = {Color.red, Color.green, Color.blue};
-
         private static readonly Color[] BaseColors = {Color.red, Color.green, Color.blue};
+        private static readonly Color[][] BaseColors2 = {BaseColors, BaseColors};
 
-        // [ShowInInspector] private static readonly Color[] StaticReadOnlyField = BaseColors;
+        [ShowInInspector] private static readonly Color[] StaticReadOnlyField = BaseColors;
         [ShowInInspector] private static readonly Color[][] StaticReadOnlyField2 =
         {
             BaseColors,
             BaseColors,
         };
 
-        // [ShowInInspector] private static  Color[] StaticProp => BaseColors;
-        [ShowInInspector] private static Color[][] StaticProp2 => new[] {
-            BaseColors,
-            BaseColors,
-        };
+        [ShowInInspector] private static  Color[] StaticProp => BaseColors;
+        [ShowInInspector] private static Color[][] StaticProp2 => BaseColors2;
     }
 }
