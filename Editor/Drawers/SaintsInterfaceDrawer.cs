6b5a87b0ff6761e42694b1915b3e71e32e61bffa
TylerTemp <tylertempdev@gmail.com>
2024-05-27 10:03:21 +0100

    fix 2021_3 import

--- Editor/Drawers/SaintsInterfaceDrawer.cs
+++ Editor/Drawers/SaintsInterfaceDrawer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using SaintsField.Editor.Utils;
-using SaintsField.Utils;
 using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
