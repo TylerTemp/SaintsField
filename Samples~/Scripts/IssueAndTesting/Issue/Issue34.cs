e777e2a7de3f88c584d51926b64980110a813709
TylerTemp <tylertempdev@gmail.com>
2024-06-09 07:37:27 +0100

    reproduce issue #34

---
+++ Samples~/Scripts/IssueAndTesting/Issue/Issue34.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaintsField.Samples.Scripts.IssueAndTesting.Issue
+{
+    public class Issue34 : MonoBehaviour
+    {
+        [SerializeReference, ReferencePicker] public TestData[] TestData;
+    }
+
+    [Serializable]
+    public class TestData
+    {
+        [SerializeReference, ReferencePicker] public InternalTestData[] InternalTestData;
+        [MinMaxSlider(0, 100)] public Vector2 TestMinMax = new Vector2(0, 2);
+    }
+
+    [Serializable]
+    public class InternalTestData
+    {
+        [MinMaxSlider(0, 100)] public Vector2 InternalTestMinMax = new Vector2(0, 2);
+    }
+}
