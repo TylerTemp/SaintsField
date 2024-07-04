20fed1ec28e73dce0f7f21f6fddd73c0eb58eee1
TylerTemp <tylertempdev@gmail.com>
2024-07-04 05:57:35 +0100

    reproduce #43

---
+++ Samples~/Scripts/IssueAndTesting/Issue/Issue43.cs
@@ -0,0 +1,10 @@
+using SaintsField.Samples.Scripts.IssueAndTesting.Issue.Issue41;
+using UnityEngine;
+
+namespace SaintsField.Samples.Scripts.IssueAndTesting.Issue
+{
+    public class Issue43 : MonoBehaviour
+    {
+        [GetComponentInParent] public MCUnit mcUnit;
+    }
+}
