86ce2e43752ec96f2f083a679bdf95a47c9649fe
TylerTemp <tylertempdev@gmail.com>
2024-07-05 11:58:45 +0100

    fix EnableIf constructor

--- Runtime/EnableIfAttribute.cs
+++ Runtime/EnableIfAttribute.cs
@@ -7,59 +7,12 @@ namespace SaintsField
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
     public class EnableIfAttribute: ReadOnlyAttribute
     {
-        public EnableIfAttribute(EMode editorMode, params string[] by) : base(editorMode, by)
+        public EnableIfAttribute(EMode editorMode, params object[] by) : base(editorMode, by)
         {
         }
 
-        public EnableIfAttribute(params string[] by) : base(by)
+        public EnableIfAttribute(params object[] by) : base(by)
         {
         }
-
-        #region Enum 1-4
-
-        public EnableIfAttribute(EMode editorMode, string callback1, object enumTarget1): base(editorMode, callback1, enumTarget1){}
-
-        public EnableIfAttribute(string callback1, object enumTarget1): base(callback1, enumTarget1){}
-
-        public EnableIfAttribute(EMode editorMode, string callback1, object enumTarget1, string callback2, object enumTarget2): base(editorMode, callback1, enumTarget1, callback2, enumTarget2){}
-
-        public EnableIfAttribute(string callback1, object enumTarget1, string callback2, object enumTarget2): base(callback1, enumTarget1, callback2, enumTarget2){}
-
-        public EnableIfAttribute(EMode editorMode, string callback1, object enumTarget1, string callback2, object enumTarget2, string callback3, object enumTarget3): base(editorMode, callback1, enumTarget1, callback2, enumTarget2, callback3, enumTarget3){}
-
-        public EnableIfAttribute(string callback1, object enumTarget1, string callback2, object enumTarget2, string callback3, object enumTarget3): base(callback1, enumTarget1, callback2, enumTarget2, callback3, enumTarget3){}
-
-        public EnableIfAttribute(EMode editorMode, string callback1, object enumTarget1, string callback2, object enumTarget2, string callback3, object enumTarget3, string callback4, object enumTarget4): base(editorMode, callback1, enumTarget1, callback2, enumTarget2, callback3, enumTarget3, callback4, enumTarget4){}
-
-        public EnableIfAttribute(string callback1, object enumTarget1, string callback2, object enumTarget2, string callback3, object enumTarget3, string callback4, object enumTarget4): base(callback1, enumTarget1, callback2, enumTarget2, callback3, enumTarget3, callback4, enumTarget4){}
-        #endregion
-
-        #region string+enum 2-4
-
-        // 1+1
-        public EnableIfAttribute(EMode editorMode, string normalCallback, string enumCallback1, object enumTarget1): base(editorMode, normalCallback, enumCallback1, enumTarget1){}
-        public EnableIfAttribute(string normalCallback, string enumCallback1, object enumTarget1): base(normalCallback, enumCallback1, enumTarget1){}
-
-        // 1+2
-        public EnableIfAttribute(EMode editorMode, string normalCallback, string enumCallback1, object enumTarget1, string enumCallback2, object enumTarget2): base(editorMode, normalCallback, enumCallback1, enumTarget1, enumCallback2, enumTarget2){}
-        public EnableIfAttribute(string normalCallback, string enumCallback1, object enumTarget1, string enumCallback2, object enumTarget2): base(normalCallback, enumCallback1, enumTarget1, enumCallback2, enumTarget2){}
-
-        // 1+3
-        public EnableIfAttribute(EMode editorMode, string normalCallback, string enumCallback1, object enumTarget1, string enumCallback2, object enumTarget2, string enumCallback3, object enumTarget3): base(editorMode, normalCallback, enumCallback1, enumTarget1, enumCallback2, enumTarget2, enumCallback3, enumTarget3){}
-        public EnableIfAttribute(string normalCallback, string enumCallback1, object enumTarget1, string enumCallback2, object enumTarget2, string enumCallback3, object enumTarget3): base(normalCallback, enumCallback1, enumTarget1, enumCallback2, enumTarget2, enumCallback3, enumTarget3){}
-
-        // 2+1
-        public EnableIfAttribute(EMode editorMode, string normalCallback1, string normalCallback2, string enumCallback1, object enumTarget1): base(editorMode, normalCallback1, normalCallback2, enumCallback1, enumTarget1){}
-        public EnableIfAttribute(string normalCallback1, string normalCallback2, string enumCallback1, object enumTarget1): base(normalCallback1, normalCallback2, enumCallback1, enumTarget1){}
-
-        // 2+2
-        public EnableIfAttribute(EMode editorMode, string normalCallback1, string normalCallback2, string enumCallback1, object enumTarget1, string enumCallback2, object enumTarget2): base(editorMode, normalCallback1, normalCallback2, enumCallback1, enumTarget1, enumCallback2, enumTarget2) {}
-        public EnableIfAttribute(string normalCallback1, string normalCallback2, string enumCallback1, object enumTarget1, string enumCallback2, object enumTarget2): base(normalCallback1, normalCallback2, enumCallback1, enumTarget1, enumCallback2, enumTarget2){}
-
-        // 3+1
-        public EnableIfAttribute(EMode editorMode, string normalCallback1, string normalCallback2, string normalCallback3, string enumCallback1, object enumTarget1): base(editorMode, normalCallback1, normalCallback2, normalCallback3, enumCallback1, enumTarget1){}
-        public EnableIfAttribute(string normalCallback1, string normalCallback2, string normalCallback3, string enumCallback1, object enumTarget1): base(normalCallback1, normalCallback2, normalCallback3, enumCallback1, enumTarget1){}
-
-        #endregion
     }
 }
--- Samples~/Scripts/IssueAndTesting/Issue/Issue4/DisabledIfPlayAttribute.cs
+++ Samples~/Scripts/IssueAndTesting/Issue/Issue4/DisabledIfPlayAttribute.cs
@@ -2,6 +2,12 @@
 {
     public class DisabledIfPlayAttribute: ReadOnlyAttribute
     {
+        public DisabledIfPlayAttribute(EMode editorMode, params object[] by) : base(editorMode, by)
+        {
+        }
 
+        public DisabledIfPlayAttribute(params object[] by) : base(by)
+        {
+        }
     }
 }
