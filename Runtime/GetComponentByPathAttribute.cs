6d5d5d52c6a4282a6b4f08053a52b939bf8fb690
TylerTemp <tylertempdev@gmail.com>
2024-09-07 11:37:10 +0100

    This shit is sooooo difficult...

--- Runtime/GetComponentByPathAttribute.cs
+++ Runtime/GetComponentByPathAttribute.cs
@@ -41,6 +41,41 @@ namespace SaintsField
         {
             Child,
             Ancestor,
+            AncestorOrSelf,
+        }
+
+        public enum IndexFilterType
+        {
+            None,
+            Number,
+            String,
+            Index,  // index()
+            Last,  // last()
+        }
+
+        public struct IndexFilter
+        {
+            public IndexFilterType IndexFilterType;
+            public string Operator;  // >, <. == etc. "" means nothing
+            public int Number;
+            public string String;
+        }
+
+        public enum NodeAttributeType
+        {
+            None,
+            AttributeSelector,  // component[index].attribute...
+            Resource,  // resource()
+            AssetDatabase,  // asset-database(),
+            Guid,  // guid(),
+        }
+
+        public struct NodeAttribute
+        {
+            public NodeAttributeType NodeAttributeType;
+            public string Value;
+            public bool IsMethod;
+            public IReadOnlyList<IndexFilter> IndexFilters;
         }
 
         public enum PredicateType
@@ -55,10 +90,13 @@ namespace SaintsField
             public string Value;
         }
 
+        // {Axes}::{Node}@{EStr}{Attribute.Property}[{Predicates}][{Predicates}]...
         public struct Token
         {
             public Axes Axes;
             public string Node;
+            public NodeAttributeType NodeAttributeType;
+            public string NodeAttribute;
             public IReadOnlyList<Predicate> Predicates;
 
             // ReSharper disable InconsistentNaming
@@ -172,7 +210,7 @@ namespace SaintsField
 
             foreach ((bool isAncestor, string step) in ChunkPath(usePath))
             {
-                (Axes axes, string node, IReadOnlyList<Predicate> predicates) = ParseStep(step);
+                (Axes axes, string node, NodeAttributeType nodeAttributeType, string nodeAttribute, IReadOnlyList<Predicate> predicates) = ParseStep(step);
             }
 
 
@@ -283,17 +321,203 @@ namespace SaintsField
             }
         }
 
-        private static (Axes axes, string node, IReadOnlyList<Predicate> predicates) ParseStep(string step)
-        {
-            Axes axes = Axes.Child;
-            string[] split = step.Split("::");
+        // AttributeSelector,  // component[index].attribute...
+        // Resource,  // Resource()
+        // AssetDatabase,  // AssetDatabase(),
+        // Guid,  // Guid(),
 
-            if (split.Length > 1)
+        private static (Axes axes, string node, NodeAttributeType nodeAttributeType, string nodeAttribute, IReadOnlyList<Predicate> predicates) ParseStep(string step)
+        {
+            List<string> splits = step.Split("::").ToList();
+            Axes axes = ExtractAxesFromStepFragments(splits);
+
+            (string node, IEnumerable<NodeAttribute> nodeAttributes) =
+                ExtractNodeInfoFromStepFragments(splits);
+            // if (split.Length > 1)
+            // {
+            //     string axesName = split[0];
+            //     string leftContent = string.Join("::", split.Skip(1));
+            // }
+
+        }
+
+        // Child,
+        // Ancestor,
+        // AncestorOrSelf,
+        private static Axes ExtractAxesFromStepFragments(List<string> splits)
+        {
+            string first = splits[0];
+            Axes axes = Axes.Child;
+            switch (first)
             {
-                string axesName = split[0];
-                string leftContent = string.Join("::", split.Skip(1));
+                case "child":
+                    break;
+                case "ancestor":
+                    axes = Axes.Ancestor;
+                    break;
+                case "ancestor-or-self":
+                    axes = Axes.AncestorOrSelf;
+                    break;
+                default:
+                    return axes;
             }
 
+            splits.RemoveAt(0);
+            return axes;
+        }
+
+        // for component:
+        // node@component.info().attr.invoke()
+        // no indexer, no function parameters
+        // node@attribute[filter][filter]
+        private static (string node, IEnumerable<NodeAttribute> nodeAttributes) ExtractNodeInfoFromStepFragments(List<string> splits)
+        {
+            if (splits.Count == 0 || splits[0].StartsWith("["))
+            {
+                return ("", Array.Empty<NodeAttribute>());
+            }
+
+            string first = splits[0];
+            splits.RemoveAt(0);
+
+            string[] splitAttribute = first.Split("@");
+            string node = splitAttribute[0];
+            if (splitAttribute.Length == 1)
+            {
+                return (node, Array.Empty<NodeAttribute>());
+            }
+
+            string nodeAttributeRaw = splitAttribute[1];
+
+            switch (nodeAttributeRaw)
+            {
+                case "":
+                    return (node, Array.Empty<NodeAttribute>());
+                case "resource()":
+                    return (node, new[]{new NodeAttribute
+                    {
+                        NodeAttributeType = NodeAttributeType.Resource,
+                        Value = "",
+                    }});
+                case "asset-database()":
+                    return (node, new []
+                    {
+                        new NodeAttribute
+                        {
+                            NodeAttributeType = NodeAttributeType.AssetDatabase,
+                            Value = "",
+                        }
+                    });
+                case "guid()":
+                    return (node, new[]{new NodeAttribute
+                    {
+                        NodeAttributeType = NodeAttributeType.Guid,
+                        Value = "",
+                    }});
+                default:
+                    return (node, ParseNodeAttributeSelector(nodeAttributeRaw));
+            }
+        }
+
+        // component.funcName()[0]["content"].value.Invoke()
+        private static IEnumerable<NodeAttribute> ParseNodeAttributeSelector(string nodeAttributeRaw)
+        {
+            foreach (string nodeAttributeSegment in nodeAttributeRaw.Split('.'))
+            {
+                (string nodeAttributeContent, bool isMethod, IReadOnlyList<IndexFilter> indexFilters) =
+                    ParseNodeWithFilter(nodeAttributeSegment);
+                yield return new NodeAttribute
+                {
+                    NodeAttributeType = NodeAttributeType.AttributeSelector,
+                    Value = nodeAttributeContent,
+                    IsMethod = isMethod,
+                    IndexFilters = indexFilters,
+                };
+            }
+        }
+
+        private static (string nodeAttributeContent, bool isMethod, IReadOnlyList<IndexFilter> indexFilters) ParseNodeWithFilter(string nodeAttributeSegment)
+        {
+            MatchCollection matches = ContentSquareBracket.Matches(nodeAttributeSegment);
+            if (matches.Count == 0)
+            {
+                (string simpleNodeContent, bool simpleNodeIsMethod)  = ParseMethodCall(nodeAttributeSegment);
+
+                return (simpleNodeContent, simpleNodeIsMethod, Array.Empty<IndexFilter>());
+            }
+
+            Match firstMatch = matches[0];
+            string rootContent = firstMatch.Groups[0].Value;
+            IndexFilter[] indexFilters = matches.Select(each => ParseIndexFilter(each.Groups[1].Value)).ToArray();
+
+            (string nodeContent, bool nodeIsMethod)  = ParseMethodCall(rootContent);
+            return (nodeContent, nodeIsMethod, indexFilters);
+        }
+
+        private static (string content, bool isMethod) ParseMethodCall(string segment)
+        {
+            string[] splitMethodCall = segment.Split("()");
+            return splitMethodCall.Length == 1
+                ? (segment, false)
+                : (splitMethodCall[0], true);
+        }
+
+        private static readonly Regex SubNumberRegex = new Regex(@"([^\d\-\.]+)(.*)");
+
+        private static IndexFilter ParseIndexFilter(string indexRaw)
+        {
+            if (indexRaw.StartsWith("\"") && indexRaw.EndsWith("\""))
+            {
+                return new IndexFilter
+                {
+                    IndexFilterType = IndexFilterType.String,
+                    String = indexRaw.Substring(1, indexRaw.Length - 2),
+                };
+            }
+            if (indexRaw.StartsWith("'") && indexRaw.EndsWith("'"))
+            {
+                return new IndexFilter
+                {
+                    IndexFilterType = IndexFilterType.String,
+                    String = indexRaw.Substring(1, indexRaw.Length - 2),
+                };
+            }
+
+            string[] bracketSplit = indexRaw.Split("()");
+
+            if (bracketSplit.Length > 1)  // index() > sth etc
+            {
+                string funcPart = bracketSplit[0].Trim();
+                if (funcPart == "last")  // [last()]
+                {
+                    return new IndexFilter
+                    {
+                        IndexFilterType = IndexFilterType.Last,
+                    };
+                }
+                Debug.Assert(funcPart == "index", indexRaw);
+
+                string comparePart = bracketSplit[1].Trim();
+                Match match = SubNumberRegex.Match(comparePart);
+                Debug.Assert(match.Success, indexRaw);
+                string rawOperator = match.Groups[0].Value.Trim();
+                int number = int.Parse(match.Groups[1].Value.Trim());
+
+                return new IndexFilter
+                {
+                    IndexFilterType = IndexFilterType.Index,
+                    Operator = rawOperator,
+                    Number = number,
+                };
+            }
+
+            // pure number
+            int pureNumber = int.Parse(indexRaw.Trim());
+            return new IndexFilter
+            {
+                IndexFilterType = IndexFilterType.Number,
+                Number = pureNumber,
+            };
         }
 
         private static IEnumerable<(bool ancestor, string segment)> ChunkPath(string usePath)
