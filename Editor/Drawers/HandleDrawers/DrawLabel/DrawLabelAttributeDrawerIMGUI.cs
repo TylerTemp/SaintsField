b8d99a9caf96ba6ec2e545b0bc419a4288bb390f
TylerTemp <tylertempdev@gmail.com>
2024-12-18 07:03:58 +0000

    IMGUI fix handles in list/array update

--- Editor/Drawers/HandleDrawers/DrawLabel/DrawLabelAttributeDrawerIMGUI.cs
+++ Editor/Drawers/HandleDrawers/DrawLabel/DrawLabelAttributeDrawerIMGUI.cs
@@ -34,104 +34,105 @@ namespace SaintsField.Editor.Drawers.HandleDrawers.DrawLabel
         {
             string key = GetKey(property);
 
-            if (!_idToLabelInfo.TryGetValue(key, out LabelInfo labelInfo))
+            if (_idToLabelInfo.TryGetValue(key, out LabelInfo labelInfo))
             {
-                DrawLabelAttribute drawLabelAttribute = (DrawLabelAttribute)saintsAttribute;
+                return position;
+            }
 
-                Util.TargetWorldPosInfo targetWorldPosInfo = Util.GetPropertyTargetWorldPosInfo(drawLabelAttribute.Space, property, info, parent);
-                if (targetWorldPosInfo.Error != "")
+            DrawLabelAttribute drawLabelAttribute = (DrawLabelAttribute)saintsAttribute;
+
+            Util.TargetWorldPosInfo targetWorldPosInfo = Util.GetPropertyTargetWorldPosInfo(drawLabelAttribute.Space, property, info, parent);
+            if (targetWorldPosInfo.Error != "")
+            {
+                Debug.LogError(targetWorldPosInfo.Error);
+                return position;
+            }
+
+            labelInfo = new LabelInfo
+            {
+                Space = drawLabelAttribute.Space,
+                Content = drawLabelAttribute.Content,
+                ActualContent = drawLabelAttribute.Content,
+                IsCallback = drawLabelAttribute.IsCallback,
+                EColor = drawLabelAttribute.EColor,
+                TargetWorldPosInfo = targetWorldPosInfo,
+                GUIStyle = drawLabelAttribute.EColor == EColor.White
+                    ? GUI.skin.label
+                    : new GUIStyle
+                    {
+                        normal = { textColor = drawLabelAttribute.EColor.GetColor() },
+                    },
+            };
+            _idToLabelInfo[key] = labelInfo;
+            SceneView.duringSceneGui += OnSceneGUIIMGUI;
+            SceneView.RepaintAll();
+
+            Selection.selectionChanged += OnSelectionChanged;
+
+            return position;
+
+            void OnSelectionChanged()
+            {
+                bool remove = false;
+                UnityEngine.Object oriObject = null;
+                try
                 {
-                    Debug.LogError(targetWorldPosInfo.Error);
-                    return position;
+                    oriObject = property.serializedObject.targetObject;
+                }
+                catch (Exception)
+                {
+                    remove = true;
                 }
 
-                labelInfo = new LabelInfo
+                if (!remove)
                 {
-                    Space = drawLabelAttribute.Space,
-                    Content = drawLabelAttribute.Content,
-                    ActualContent = drawLabelAttribute.Content,
-                    IsCallback = drawLabelAttribute.IsCallback,
-                    EColor = drawLabelAttribute.EColor,
-                    TargetWorldPosInfo = targetWorldPosInfo,
-                    GUIStyle = drawLabelAttribute.EColor == EColor.White
-                        ? GUI.skin.label
-                        : new GUIStyle
-                        {
-                            normal = { textColor = drawLabelAttribute.EColor.GetColor() },
-                        },
-                };
-                _idToLabelInfo[key] = labelInfo;
-                SceneView.duringSceneGui += OnSceneGUIIMGUI;
-                SceneView.RepaintAll();
-
-                Selection.selectionChanged += OnSelectionChanged;
-
-                return position;
-
-                void OnSelectionChanged()
-                {
-                    bool remove = false;
-                    UnityEngine.Object oriObject = null;
-                    try
-                    {
-                        oriObject = property.serializedObject.targetObject;
-                    }
-                    catch (Exception)
+                    if (oriObject == null)
                     {
                         remove = true;
                     }
-
-                    if (!remove)
+                    else
                     {
-                        if (oriObject == null)
-                        {
-                            remove = true;
-                        }
-                        else
-                        {
-                            remove = Array.IndexOf(Selection.objects, oriObject) == -1;
-                        }
-                    }
-
-                    if (remove)
-                    {
-                        Unsub();
+                        remove = Array.IndexOf(Selection.objects, oriObject) == -1;
                     }
                 }
-            }
 
-            if (!labelInfo.TargetWorldPosInfo.IsTransform)
-            {
-                labelInfo.TargetWorldPosInfo = Util.GetPropertyTargetWorldPosInfo(labelInfo.Space, property, info, parent);
+                if (remove)
+                {
+                    Unsub();
+                }
             }
-
-            if (!labelInfo.IsCallback)
-            {
-                return position;
-            }
-
-            (string valueError, object value) = Util.GetOf<object>(labelInfo.Content, null, property, info, parent);
-            if (valueError != "")
-            {
-                Debug.LogError(valueError);
-                return position;
-            }
-
-            if (value is IWrapProp wrapProp)
-            {
-                value = Util.GetWrapValue(wrapProp);
-            }
-
-            labelInfo.ActualContent = $"{value}";
-            return position;
 
             // ReSharper disable once InconsistentNaming
             void OnSceneGUIIMGUI(SceneView sceneView)
             {
-                if (_idToLabelInfo.TryGetValue(key, out LabelInfo cachedLabelInfo))
+                if (!_idToLabelInfo.TryGetValue(key, out LabelInfo cachedLabelInfo))
                 {
-                    OnSceneGUIInternal(sceneView, cachedLabelInfo);
+                    return;
                 }
+
+                if (!cachedLabelInfo.TargetWorldPosInfo.IsTransform)
+                {
+                    cachedLabelInfo.TargetWorldPosInfo = Util.GetPropertyTargetWorldPosInfo(cachedLabelInfo.Space, property, info, parent);
+                }
+
+                if (cachedLabelInfo.IsCallback)
+                {
+                    (string valueError, object value) =
+                        Util.GetOf<object>(cachedLabelInfo.Content, null, property, info, parent);
+                    if (valueError != "")
+                    {
+                        Debug.LogError(valueError);
+                        return;
+                    }
+
+                    if (value is IWrapProp wrapProp)
+                    {
+                        value = Util.GetWrapValue(wrapProp);
+                    }
+                    labelInfo.ActualContent = $"{value}";
+                }
+
+                OnSceneGUIInternal(sceneView, cachedLabelInfo);
             }
 
             void Unsub()
