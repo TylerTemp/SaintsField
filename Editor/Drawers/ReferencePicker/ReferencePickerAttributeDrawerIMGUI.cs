2ebb131e06a6345ab6428ceae040318c634afa7f
TylerTemp <tylertempdev@gmail.com>
2024-12-18 07:37:55 +0000

    fix for old unity

--- Editor/Drawers/ReferencePicker/ReferencePickerAttributeDrawerIMGUI.cs
+++ Editor/Drawers/ReferencePicker/ReferencePickerAttributeDrawerIMGUI.cs
@@ -1,3 +1,4 @@
+#if UNITY_2021_3_OR_NEWER
 using System;
 using System.Reflection;
 using UnityEditor;
@@ -94,3 +95,4 @@ namespace SaintsField.Editor.Drawers.ReferencePicker
         #endregion
     }
 }
+#endif
