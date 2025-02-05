ec0295990a2883fbacbcdfb1b2d5c1bc68988db2
tyler <tyler@example.com>
2025-02-05 07:11:49 +0000

    try re-produce #132

---
+++ Samples~/Scripts/IssueAndTesting/Issue/Issue132/Issue132Base.cs
@@ -0,0 +1,15 @@
+using SaintsField.Samples.Scripts.IssueAndTesting.Issue46;
+using SaintsField.Samples.Scripts.IssueAndTesting.Issue62;
+using SaintsField.Samples.Scripts.SaintsEditor;
+using UnityEngine;
+
+namespace SaintsField.Samples.Scripts.IssueAndTesting.Issue.Issue132
+{
+    public class Issue132Base : SaintsMonoBehaviour
+    {
+        public virtual MCDamageHandler realSelf => null;
+        public virtual bool            isRelay  => false;
+
+        public virtual MCTeam          team   => null;
+    }
+}
---
+++ Samples~/Scripts/IssueAndTesting/Issue/Issue132/Issue132Error.cs
@@ -0,0 +1,30 @@
+using SaintsField.Playa;
+using SaintsField.Samples.Scripts.IssueAndTesting.Issue46;
+using SaintsField.Samples.Scripts.IssueAndTesting.Issue62;
+using UnityEngine;
+
+namespace SaintsField.Samples.Scripts.IssueAndTesting.Issue.Issue132
+{
+    public class Issue132Error : Issue132Base
+    {
+        public override  MCDamageHandler realSelf => parent;
+        public override bool            isRelay  => true;
+
+        [LayoutGroup(" ------ CHILD -----",ELayout.Title|ELayout.Background),
+         GetComponentInParents(excludeSelf: true),
+         HideIf(nameof(_parentOverride)),
+        ]
+        // ReSharper disable once InconsistentNaming
+        public MCDamageHandler _parent;
+
+        [Tooltip("set this to override the auto parent")]
+        // ReSharper disable once InconsistentNaming
+        public MCDamageHandler _parentOverride;
+        // ReSharper disable once InconsistentNaming
+        // ReSharper disable once MemberCanBePrivate.Global
+        public          MCDamageHandler parent => _parentOverride ? _parentOverride : _parent;
+        public override MCTeam          team   => parent?.team;
+        // 0 = totally immune to damage
+        public float damageRatio = 1;
+    }
+}
