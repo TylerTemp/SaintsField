2d8b1c4b5e747669f5d4389371f065b4dd8367bb
TylerTemp <tylertempdev@gmail.com>
2025-03-01 03:54:02 +0000

    re-produce #155

---
+++ Samples~/Scripts/IssueAndTesting/Issue/Issue155/Issue155SaintsDraw.cs
@@ -0,0 +1,9 @@
+using SaintsField.Samples.Scripts.SaintsEditor;
+
+namespace SaintsField.Samples.Scripts.IssueAndTesting.Issue.Issue155
+{
+    public class Issue155SaintsDraw : SaintsMonoBehaviour
+    {
+        public SceneReference sceneReference;
+    }
+}
