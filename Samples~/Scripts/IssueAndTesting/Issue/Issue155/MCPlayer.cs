76e017fa2264f6c9e37f281be4e7d59484b1ea5f
TylerTemp <tylertempdev@gmail.com>
2025-04-05 15:38:47 +0100

    remove code from issue

--- Samples~/Scripts/IssueAndTesting/Issue/Issue155/MCPlayer.cs
+++ Samples~/Scripts/IssueAndTesting/Issue/Issue155/MCPlayer.cs
@@ -7,12 +7,12 @@ namespace SaintsField.Samples.Scripts.IssueAndTesting.Issue.Issue155
 	public class MCPlayer: SaintsMonoBehaviour
 	{
 
-		public SceneReference sceneReference;
+		// public SceneReference sceneReference;
+		//
+		// [ShowInInspector] private SceneReference sceneReference1 => null;
+		// [ShowInInspector] private SceneReference sceneReference2 => sceneReference;
 
-		[ShowInInspector] private SceneReference sceneReference1 => null;
-		[ShowInInspector] private SceneReference sceneReference2 => sceneReference;
-
-		[Button]
-		private void SceneRefButton(SceneReference sr) => Debug.Log(sr);
+		// [Button]
+		// private void SceneRefButton(SceneReference sr) => Debug.Log(sr);
 	}
 }
