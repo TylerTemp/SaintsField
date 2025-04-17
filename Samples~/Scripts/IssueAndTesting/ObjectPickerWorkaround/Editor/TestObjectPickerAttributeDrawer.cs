952deab36a596e1e4ffb63c02d827baf4d870b07
TylerTemp <tylertempdev@gmail.com>
2025-04-17 09:52:29 +0100

    wip #204

--- Samples~/Scripts/IssueAndTesting/ObjectPickerWorkaround/Editor/TestObjectPickerAttributeDrawer.cs
+++ Samples~/Scripts/IssueAndTesting/ObjectPickerWorkaround/Editor/TestObjectPickerAttributeDrawer.cs
@@ -6,10 +6,6 @@ using SaintsField.Interfaces;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
-using PopupWindow = UnityEditor.PopupWindow;
-using UnityEditor;
-using UnityEditor.UIElements;
-using UnityEngine.UIElements;
 
 namespace SaintsField.Samples.Scripts.IssueAndTesting.ObjectPickerWorkaround.Editor
 {
@@ -17,85 +13,67 @@ namespace SaintsField.Samples.Scripts.IssueAndTesting.ObjectPickerWorkaround.Edi
 
     public class PopupContentExample : EditorWindow
     {
-        // public override void OnOpen()
-        // {
-        //     Debug.Log("Popup opened: " + this);
-        //     VisualTreeAsset visualTreeAsset = Util.LoadResource<VisualTreeAsset>("UIToolkit/ObjectPicker/ObjectPickerPanel.uxml");
-        //     visualTreeAsset.CloneTree(editorWindow.rootVisualElement);
-        //
-        // }
-        //
-        // // public override VisualElement CreateGUI()
-        // // {
-        // // }
-        //
-        // public override void OnGUI(Rect rect)
-        // {
-        //     // Intentionally left empty
-        // }
-        //
-        // public override void OnClose()
-        // {
-        //     Debug.Log("Popup closed: " + this);
-        // }
+        private VisualElement _pickerBody;
+        private ListView _listView;
+        private ScrollView _blockView;
+
+        private Image _loadingImage;
+
         public void CreateGUI()
         {
-            // Get a list of all sprites in the project
-            // var allObjectGuids = AssetDatabase.FindAssets("t:Sprite");
-            // var allObjects = new List<Sprite>();
-            // foreach (var guid in allObjectGuids)
-            // {
-            //     allObjects.Add(AssetDatabase.LoadAssetAtPath<Sprite>(AssetDatabase.GUIDToAssetPath(guid)));
-            // }
             VisualTreeAsset visualTreeAsset = Util.LoadResource<VisualTreeAsset>("UIToolkit/ObjectPicker/ObjectPickerPanel.uxml");
             visualTreeAsset.CloneTree(rootVisualElement);
-            // rootVisualElement.Q<ToolbarSearchField>().style.width = StyleKeyword.Initial;
+
+            _loadingImage = rootVisualElement.Q<Image>(name: "saints-field-object-picker-loading");
+            _loadingImage.image = Util.LoadResource<Texture2D>("refresh.png");
+            Debug.Log(_loadingImage.image);
+            UIToolkitUtils.KeepRotate(_loadingImage);
+            // _loadingImage.RegisterCallback<AttachToPanelEvent>(_ =>  UIToolkitUtils.TriggerRotate(_loadingImage));
+            _loadingImage.schedule.Execute(() => UIToolkitUtils.TriggerRotate(_loadingImage)).StartingIn(300);
+            // UIToolkitUtils.TriggerRotate(_loadingImage);
+            // buttonRotator.schedule.Execute(() => UIToolkitUtils.TriggerRotate(buttonRotator));
+
+            _pickerBody = rootVisualElement.Q<VisualElement>("saints-field-object-picker-body");
+
+            VisualTreeAsset listItemAsset = Util.LoadResource<VisualTreeAsset>("UIToolkit/ObjectPicker/ObjectPickerListItem.uxml");
+            Debug.Assert(listItemAsset != null);
+            _listView = _pickerBody.Q<ListView>(name: "saints-field-object-picker-list");
+            Debug.Assert(_listView != null);
+            VisualElement listViewContainer = _listView.Q<VisualElement>(name: "unity-content-container");
+            Debug.Assert(listViewContainer != null);
+
+            for (int i = 0; i < 15; i++)
+            {
+                VisualElement listItem = listItemAsset.CloneTree();
+                listViewContainer.Add(listItem);
+                // listItem.style.width = new StyleLength(Length.Percent(50));
+                listItem.Q<Label>(name: "saints-field-object-picker-list-item-label").text = $"Item {i}";
+            }
+
+            // _listView.RemoveFromHierarchy();
+
+            VisualTreeAsset blockItemAsset = Util.LoadResource<VisualTreeAsset>("UIToolkit/ObjectPicker/ObjectPickerBlockItem.uxml");
+            Debug.Assert(blockItemAsset != null);
+            _blockView = _pickerBody.Q<ScrollView>(name: "saints-field-object-picker-block");
+            Debug.Assert(_blockView != null);
+            VisualElement blockViewContainer = _blockView.Q<VisualElement>(name: "unity-content-container");
+            Debug.Assert(blockViewContainer != null);
+
+            blockViewContainer.style.flexDirection = FlexDirection.Row;
+            blockViewContainer.style.flexWrap = Wrap.Wrap;
+
+            _blockView.RemoveFromHierarchy();
+
+            for (int i = 0; i < 15; i++)
+            {
+                VisualElement blockItem = blockItemAsset.CloneTree();
+                // blockItem.style.width = 60;
+                // blockItem.style.height = 60;
+                blockViewContainer.Add(blockItem);
+                blockItem.Q<Label>(name: "saints-field-object-picker-block-item-name").text = $"Item {i}";
+            }
         }
     }
-
-    // public class PopupExample : EditorWindow
-    // {
-    //     //Add menu item
-    //     [MenuItem("Examples/Popup Example")]
-    //     static void Init()
-    //     {
-    //         EditorWindow window = EditorWindow.CreateInstance<PopupExample>();
-    //         window.Show();
-    //     }
-    //
-    //     private void CreateGUI()
-    //     {
-    //         var visualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/ui-toolkit-manual-code-examples/create-a-popup-window/PopupExample.uxml");
-    //         visualTreeAsset.CloneTree(rootVisualElement);
-    //
-    //         var button = rootVisualElement.Q<Button>();
-    //         button.clicked += () => PopupWindow.Show(button.worldBound, new PopupContentExample());
-    //     }
-    // }
-
-    // public class PopupContentExample : PopupWindowContent
-    // {
-    //     public override void OnGUI(Rect rect)
-    //     {
-    //         throw new System.NotImplementedException();
-    //     }
-    //
-    //     public override void OnOpen()
-    //     {
-    //         Debug.Log("Popup opened: " + this);
-    //     }
-    //
-    //     public override VisualElement CreateGUI()
-    //     {
-    //         var visualTreeAsset = Util.LoadResource<VisualTreeAsset>("UIToolkit/ObjectPicker/ObjectPickerPanel.uxml");
-    //         return visualTreeAsset.CloneTree();
-    //     }
-    //
-    //     public override void OnClose()
-    //     {
-    //         Debug.Log("Popup closed: " + this);
-    //     }
-    // }
 
     [CustomPropertyDrawer(typeof(TestObjectPickerAttribute), true)]
     public class TestObjectPickerAttributeDrawer : SaintsPropertyDrawer
