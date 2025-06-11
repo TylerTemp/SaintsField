59e71eb126307fb93bddf7e8e93afd016528ce49
tyler <tyler@example.com>
2025-06-11 14:07:21 +0100

    fix for old unity

--- Editor/Playa/ISaintsRenderer.cs
+++ Editor/Playa/ISaintsRenderer.cs
@@ -10,7 +10,7 @@ namespace SaintsField.Editor.Playa
 #if UNITY_2021_3_OR_NEWER // && !SAINTSFIELD_UI_TOOLKIT_DISABLE
         VisualElement CreateVisualElement();
 #endif
-        public bool InAnyHorizontalLayout { get; set; }
+        bool InAnyHorizontalLayout { get; set; }
 
         void RenderIMGUI(float width);
 
