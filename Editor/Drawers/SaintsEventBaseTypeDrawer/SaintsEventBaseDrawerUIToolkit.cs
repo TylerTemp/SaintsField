95c5f5c9dbb6993e87d2c2a6a8d731ced2934a8d
TylerTemp <tylertempdev@gmail.com>
2025-07-16 12:37:03 +0100

    fix missing package

--- Editor/Drawers/SaintsEventBaseTypeDrawer/SaintsEventBaseDrawerUIToolkit.cs
+++ Editor/Drawers/SaintsEventBaseTypeDrawer/SaintsEventBaseDrawerUIToolkit.cs
@@ -1,3 +1,4 @@
+#if SAINTSFIELD_SERIALIZATION && SAINTSFIELD_SERIALIZATION_ENABLE
 using System;
 using System.Collections.Generic;
 using System.Reflection;
@@ -99,3 +100,4 @@ namespace SaintsField.Editor.Drawers.SaintsEventBaseTypeDrawer
         }
     }
 }
+#endif
