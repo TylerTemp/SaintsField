48c4c0a645ba0a690ce109e486e7b809107834ab
TylerTemp <tylertempdev@gmail.com>
2025-07-25 11:39:17 +0100

    event invoke

--- Runtime/Events/SaintsEvent`4.cs
+++ Runtime/Events/SaintsEvent`4.cs
@@ -6,9 +6,9 @@ namespace SaintsField.Events
     [Serializable]
     public class SaintsEvent<T0, T1, T2, T3>: SaintsEventBase
     {
-        public void Invoke(T0 arg0, T1 arg1)
+        public void Invoke(T0 arg0, T1 arg1, T3 arg2, T3 arg3)
         {
-            object[] args = { arg0, arg1 };
+            object[] args = { arg0, arg1, arg2, arg3 };
             foreach (PersistentCall presistentCall in _persistentCalls)
             {
                 presistentCall.Invoke(args);
