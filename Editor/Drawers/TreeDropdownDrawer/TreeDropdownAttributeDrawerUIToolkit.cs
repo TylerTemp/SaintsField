782c2c65580073192f76cf2af8ff6cb84c4126a3
tyler <tyler@example.com>
2025-08-18 11:19:17 +0100

    fix for old unity

--- Editor/Drawers/TreeDropdownDrawer/TreeDropdownAttributeDrawerUIToolkit.cs
+++ Editor/Drawers/TreeDropdownDrawer/TreeDropdownAttributeDrawerUIToolkit.cs
@@ -1,3 +1,4 @@
+#if UNITY_2021_3_OR_NEWER
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -48,3 +49,4 @@ namespace SaintsField.Editor.Drawers.TreeDropdownDrawer
         }
     }
 }
+#endif
