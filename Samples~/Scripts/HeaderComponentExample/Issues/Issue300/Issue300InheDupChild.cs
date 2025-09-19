1f7314eb1caf7efd7161139bc4d560bd01192f71
TylerTemp <tylertempdev@gmail.com>
2025-09-19 04:21:19 +0100

    Unable to re-produce #300

---
+++ Samples~/Scripts/HeaderComponentExample/Issues/Issue300/Issue300InheDupChild.cs
@@ -0,0 +1,6 @@
+namespace SaintsField.Samples.Scripts.HeaderComponentExample.Issues.Issue300
+{
+    public class Issue300InheDupChild : Issue300InheDupParent
+    {
+    }
+}
---
+++ Samples~/Scripts/HeaderComponentExample/Issues/Issue300/Issue300InheDupParent.cs
@@ -0,0 +1,26 @@
+using SaintsField.ComponentHeader;
+using UnityEditor;
+using UnityEngine;
+
+namespace SaintsField.Samples.Scripts.HeaderComponentExample.Issues.Issue300
+{
+    public class Issue300InheDupParent : SaintsMonoBehaviour
+    {
+        [Range(0, 1)] public float normalizedHealth;
+
+#if UNITY_EDITOR
+        [HeaderDraw]
+        private HeaderUsed HeaderDrawRight1G1(HeaderArea headerArea)
+        {
+            // this is drawing from right to left, so we need to backward the rect space
+            // if(isRelay) return new HeaderUsed();
+
+            Rect useRect = new Rect(headerArea.MakeXWidthRect(headerArea.GroupStartX-50,50)) { y = headerArea.Y+2,height = headerArea.Height-4, };
+            Rect progressRect = new Rect(useRect) { width = normalizedHealth*useRect.width, };
+            EditorGUI.DrawRect(useRect,Color.gray);
+            EditorGUI.DrawRect(progressRect,Color.yellow);
+            return new HeaderUsed(useRect);
+        }
+#endif
+    }
+}
