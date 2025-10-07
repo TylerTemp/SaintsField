d647a89e1e2c60ddf1165f27b629063fc5d59e41
TylerTemp <tylertempdev@gmail.com>
2025-10-07 20:44:00 +0100

    reproduce #323

---
+++ Samples~/Scripts/SaintsEditor/Issues/Issue323.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SaintsField.Samples.Scripts.SaintsEditor.Issues
+{
+    public class Issue323 : SaintsMonoBehaviour
+    {
+        [Serializable]
+        public class NamedEvent
+        {
+            public string name = "Default Event";
+            public UnityEvent eventToTrigger = new UnityEvent();
+        }
+
+        [Tooltip("Set the events appointed here. Timeline markers will shoot events based on the corresponding 'event identifier'.")]
+        public List<NamedEvent> namedEvents = new List<NamedEvent>();
+        public List<UnityEvent> EventsTest;
+        public UnityEvent EventTest;
+    }
+}
