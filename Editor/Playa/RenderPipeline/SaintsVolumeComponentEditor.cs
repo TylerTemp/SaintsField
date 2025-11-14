562df8ca8377efc7b5386253b95bffdb255aca89
TylerTemp <tylertempdev@gmail.com>
2025-11-14 08:20:41 +0000

    #339

---
+++ Editor/Playa/RenderPipeline/SaintsVolumeComponentEditor.cs
@@ -0,0 +1,13 @@
+// using UnityEditor.Rendering;
+//
+// namespace SaintsField.Editor.Playa.RenderPipeline
+// {
+//     public class SaintsVolumeComponentEditor: SaintsEditor
+//     {
+//         public void T()
+//         {
+//             VolumeComponentEditor t = CreateEditor(this.targets, typeof(VolumeComponentEditor)) as VolumeComponentEditor;
+//
+//         }
+//     }
+// }
