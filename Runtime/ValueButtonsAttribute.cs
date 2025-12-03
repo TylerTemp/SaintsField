b1022da8ea2358a67f98414ee289e43da9445faf
TylerTemp <tylertempdev@gmail.com>
2025-12-03 08:56:51 +0000

    fix for old unity

--- Runtime/ValueButtonsAttribute.cs
+++ Runtime/ValueButtonsAttribute.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Diagnostics;
 
 // ReSharper disable once CheckNamespace
 namespace SaintsField
 {
     [Conditional("UNITY_EDITOR")]
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
     public class ValueButtonsAttribute: PathedDropdownAttribute
     {
         public ValueButtonsAttribute(string funcName = null, EUnique unique = EUnique.None): base(funcName, unique)
