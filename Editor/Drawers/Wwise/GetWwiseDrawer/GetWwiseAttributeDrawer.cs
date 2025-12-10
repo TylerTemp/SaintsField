bffc9d8576f2475dfa930225b3cd1c83d6fc42d6
TylerTemp <tylertempdev@gmail.com>
2025-12-10 08:03:18 +0000

    fix wwise getter

--- Editor/Drawers/Wwise/GetWwiseDrawer/GetWwiseAttributeDrawer.cs
+++ Editor/Drawers/Wwise/GetWwiseDrawer/GetWwiseAttributeDrawer.cs
@@ -345,7 +345,7 @@ namespace SaintsField.Editor.Drawers.Wwise.GetWwiseDrawer
                     // Debug.Log($"xPathInfo.IsCallback={xPathInfo.IsCallback}/{xPathInfo.Callback}");
                     if (xPathInfo.IsCallback)
                     {
-                        (string error, string xPathString) = Util.GetOf(xPathInfo.Callback, "", property, info, parent);
+                        (string error, string xPathString) = Util.GetOf(xPathInfo.Callback, "", property, info, parent, null);
 
                         if (error != "")
                         {
