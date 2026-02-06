b794f4a320a0bcffda8f195421e7737e2a661b9b
TylerTemp <tylertempdev@gmail.com>
2026-02-06 03:58:05 +0000

    urp marco

--- Samples~/EditorTest/UniversalRendererDataDirectlyInject.cs
+++ Samples~/EditorTest/UniversalRendererDataDirectlyInject.cs
@@ -1,4 +1,4 @@
-#if UNITY_EDITOR  && SAINTSFIELD_DEBUG
+#if UNITY_EDITOR  && SAINTSFIELD_DEBUG && SAINTSFIELD_RENDER_PIPELINE_UNIVERSAL
 using SaintsField.Editor.Playa.ScriptableRenderer;
 using UnityEditor;
 using UnityEngine.Rendering.Universal;
