e8e22081acc05bb4968ee1bc9630c3cb27851860
TylerTemp <tylertempdev@gmail.com>
2026-04-08 11:31:55 +0100

    reproduce #366

--- Samples~/Scripts/IssueAndTesting/Issue/Issue366NestedFoldoutBox.cs
+++ Samples~/Scripts/IssueAndTesting/Issue/Issue366NestedFoldoutBox.cs
@@ -1,11 +1,34 @@
+using System;
 using SaintsField.Playa;
+using UnityEngine;
 
 namespace SaintsField.Samples.Scripts.IssueAndTesting.Issue
 {
     public class Issue366NestedFoldoutBox : SaintsMonoBehaviour
     {
-        [LayoutStart("Main", ELayout.FoldoutBox)]
-        [LayoutStart("./Sub", ELayout.FoldoutBox)]
-        public string sub1;
+        [LayoutStart("various", ELayout.CollapseBox)]
+        public string various;
+
+        [Serializable]
+        public struct SpawnByTime
+        {
+            public float time;
+        }
+
+        [LayoutStart("Spawner", ELayout.FoldoutBox | ELayout.Collapse)]
+        public bool isActive;
+        public bool isFriendly;
+
+
+        [LayoutStart("Spawner by time", ELayout.FoldoutBox | ELayout.Collapse)]
+        [Separator("Spawner by time (Always Active)")]
+        [SaintsRow(inline: true)]
+        public SpawnByTime spawnByTime;
+
+        [LayoutStart("Spawner by time - when took one damage", ELayout.FoldoutBox | ELayout.Collapse)]
+        [Separator("Spawner by time (Activated on Damage)")]
+        [Tooltip("Bu SpawnByTime")]
+        [ReadOnly]
+        public bool hasTakenDamage;
     }
 }
