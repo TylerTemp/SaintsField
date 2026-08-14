f2f59324a59289f307f940fddd2b2e44bb979c7a
TylerTemp <tylertempdev@gmail.com>
2026-08-14 10:32:38 +0100

    fix for old unity

--- Editor/Core/SaintsLifecycleManagement.cs
+++ Editor/Core/SaintsLifecycleManagement.cs
@@ -4,16 +4,19 @@ using UnityEditor;
 
 namespace SaintsField.Editor.Core
 {
+    // ReSharper disable once PartialTypeWithSinglePart
     public partial class SaintsLifecycleManagement
     {
         public static readonly UnityEvent OnCodeUnloadingEvent = new UnityEvent();
 
-#if UNITY_6000_5_OR_NEWER
-        [Unity.Scripting.LifecycleManagement.OnCodeUnloading]
         private static void OnCodeUnloading()
         {
             OnCodeUnloadingEvent.Invoke();
         }
+
+#if UNITY_6000_5_OR_NEWER
+        [Unity.Scripting.LifecycleManagement.OnCodeUnloading]
+        private static void LifecycleManagementOnCodeUnloading() => OnCodeUnloading();
 #else
         [InitializeOnLoadMethod]
         private static void RegisterAssemblyCacheCleanup()
